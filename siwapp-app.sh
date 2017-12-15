4dc47d9774722ce553139668a60249a2a5f68843
Brandon Beck <robbeck@cisco.com>
2017-12-15 13:30:12 -0600

    changed vars

--- siwapp-app.sh
+++ siwapp-app.sh
@@ -21,11 +21,11 @@ chown -R apache:apache /var/www/html/
 
 sed -i -e '57,63d' /var/www/html/web/pre_installer_code.php
 
-sed -i "s/80/${APP_PORT}/" /etc/httpd/conf/httpd.conf
+sed -i "s/80/$APP_PORT/" /etc/httpd/conf/httpd.conf
 sed -i "s/LogFormat \"%h/LogFormat \"%a/g" /etc/httpd/conf/httpd.conf
 
 NODE_HOSTNAME = $(hostname)
-sed -i "21s%.*%${NODE_HOSTNAME}%g" /var/www/html/apps/siwapp/templates/layout.php
+sed -i "21s%.*%$NODE_HOSTNAME%g" /var/www/html/apps/siwapp/templates/layout.php
 
 echo $'<Directory /var/www/html/web>
 	Options FollowSymLinks
@@ -43,16 +43,16 @@ all:
     class: sfDoctrineDatabase
     param:
       dsn: 'mysql:host=${HAPROXY_DB_IP};dbname=siwapp'
-      username: '${GALERA_DB_USER}'
-      password: '${GALERA_DB_USER_PWD}'
+      username: '$GALERA_DB_USER'
+      password: '$GALERA_DB_USER_PWD'
 
 test:
   doctrine:
     class: sfDoctrineDatabase
     param:
       dsn: 'mysql:host=${HAPROXY_DB_IP};dbname=siwapp_test'
-      username: '${GALERA_DB_USER}'
-      password: '${GALERA_DB_USER_PWD}'
+      username: '$GALERA_DB_USER'
+      password: '$GALERA_DB_USER_PWD'
 EOF
 
 sed -i.bak "s#false#true#g" /var/www/html/web/config.php
