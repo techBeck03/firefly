9600a69678c0e8ccd8c40e10747be1e330f021c9
Brandon Beck <robbeck@cisco.com>
2018-01-03 14:07:04 -0600

    added haproxy and siwapp ppt

--- scriptCommands.sh
+++ scriptCommands.sh
@@ -1,5 +1,7 @@
+#sed -i 's/enforcing/permissive/g' /etc/selinux/config /etc/selinux/config
+#reboot
 export FILE_SERVER="https://raw.githubusercontent.com/techBeck03/firefly/master"
-export POD="1"
+export POD="3"
 export TIER="load-sim"
 curl -o /tmp/siwapp-${TIER}.sh ${FILE_SERVER}/siwapp-${TIER}.sh
 chmod +x /tmp/siwapp-${TIER}.sh
