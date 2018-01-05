1d1e53b3a6677749b4dad4a9c984752fc58bdbcd
Brandon Beck <robbeck@cisco.com>
2018-01-05 07:10:00 -0600

    updated deploy script

--- deploy-siwapp.ps1
+++ deploy-siwapp.ps1
@@ -9,21 +9,24 @@ Param(
     [Parameter(Mandatory=$True)]
     [string]$csv,
     [Parameter(Mandatory=$True)]
-    [string]$rootPassword
+    [string]$rootPassword,
+    [Parameter(Mandatory=$True)]
+    [string]$pod
 )
 
 # Global Variables
-$FILE_SERVER = "https://something.com"
+$FILE_SERVER = "https://raw.githubusercontent.com/techBeck03/firefly/master"
 $APP_TIERS = @{}
-$APP_TIERS."HAPROXY_APP" = "siwapp-haproxy-app.sh"
+$APP_TIERS."HAPROXY-APP" = "siwapp-haproxy-app.sh"
 $APP_TIERS."APP" = "siwapp-app.sh"
-$APP_TIERS."HAPROXY_DB" = "siwapp-haproxy-db.sh"
+$APP_TIERS."HAPROXY-DB" = "siwapp-haproxy-db.sh"
 $APP_TIERS."DB" = "siwapp-db.sh"
-$APP_TIERS."LOAD_SIM" = "siwapp-load-sim.sh"
+$APP_TIERS."LOAD-SIM" = "siwapp-load-sim.sh"
 
-Import-Csv $csv | % {
+Import-Csv $csv | where {$_.POD -eq $pod} | % {
     $target = $_
-    $deployScript = $APPTIERS[$target."APP_TIER".ToLower()]
+    
+    $deployScript = $APP_TIERS[$target."APP_TIER".ToLower()]
     Try
     {
         $output = (Invoke-VMScript -Vm $target["VMNAME"] -ScriptText "export FILE_SERVER=$FILE_SERVER;export POD=$($target["POD"]);curl -o /tmp/$deployScript ${FILE_SERVER}/$deployScript" -GuestUser "root" -GuestPassword $rootPassword -ErrorAction Stop).ScriptOutput
